59dc9585e1da22d498211bd91dbfed1e045a7830
René Descartes <kenddygabriel841@gmail.com>
2025-12-04 18:01:43 -0300

    terminar bônus

--- src/telas/Telainicial.js
+++ src/telas/Telainicial.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, Image, Dimensions, StyleSheet, FlatList, ActivityIndicator, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Dimensions, StyleSheet, FlatList, ActivityIndicator, Alert, TextInput } from 'react-native';
 import api from "../axios/api.js"
 
 const windowWidth = Dimensions.get('window').width;
@@ -8,6 +8,7 @@ const windowHeight = Dimensions.get('window').height;
 const Telainicial = ({navigation}) => {
     let [pag, setpag] = useState(1);
     const [personagens, setpersonagens] = useState([]);
+    let [filtro, setfiltro] = useState("")
 
     const IrDetalhes = (id) => {
         navigation.navigate("Detalhes", { id: id });
@@ -25,13 +26,29 @@ const Telainicial = ({navigation}) => {
         }
     }
 
-    useEffect(() => {
-        api.get(`/?page=${pag}`)
-            .then((resposta) => setpersonagens(resposta.data.results))
-            .catch((erro) => console.log("Deu erro nisso aqui: " + erro))
-        }, [pag]);
+    const filtrar = (texto) => {
+        if((isNaN(texto))&&(texto.length>0)){
+            setfiltro(`&name=${encodeURIComponent(texto)}`);
+        }
+    }
 
-    while(personagens.length==0){
+    useEffect(() => {
+        // if(isNaN(pag)){
+        //     setfiltro("/?name=");
+        // }
+
+        api.get(`/?page=${pag}${filtro}`)
+            .then((resposta) => {if(resposta.data.results){
+                setpersonagens(resposta.data.results); 
+                console.log(`https://rickandmortyapi.com/api/character/?page=${pag}${filtro}`)
+            }})
+            .catch((erro) => {
+                console.log("Deu erro nisso aqui ó: " + erro);
+                console.log(`https://rickandmortyapi.com/api/character/?page=${pag}${filtro}`);
+            })
+        }, [pag, filtro]);
+
+    while(personagens.length===0){
         return (
             <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                 <ActivityIndicator size="large" color="#000" />
@@ -43,12 +60,13 @@ const Telainicial = ({navigation}) => {
     return (
         <View style={[{flexDirection: 'column', flex: 1, padding: 20, gap: 15}]}>
             <Text style={[{fontSize: 40}]}>Personagens</Text>
+            <TextInput placeholder="Pesquisar por nome" onSubmitEditing={(texto) => filtrar(texto.nativeEvent.text)} style={[{backgroundColor: "#e6e6e6", padding: 5}]}></TextInput>
             <FlatList
                 data={personagens}
                 keyExtractor={item => item.id}
                 renderItem={({item}) => (
                     <TouchableOpacity onPress={() => IrDetalhes(item.id)}>
-                        <View style={[{flexDirection: 'row', backgroundColor: '#89b8f5', marginBottom: windowHeight*0.02, padding: 10, width: "100%"}]}>
+                        <View style={[{flexDirection: 'row', backgroundColor: '#5CAD4A', marginBottom: windowHeight*0.02, padding: 10, width: "100%"}]}>
                             <View style={[{marginRight: 10}]}>
                                 <Image source={{ uri: item.image}} style={{width: windowWidth*0.3, height: windowWidth*0.3, resizeMode: 'contain', borderRadius:windowWidth*0.1}}/>
                             </View>
@@ -65,12 +83,12 @@ const Telainicial = ({navigation}) => {
             />
             <View style={[{flexDirection: 'row', justifyContent:"center", width: windowWidth, alignContent:"center", marginLeft: -50, gap: 100}]}>
                 <TouchableOpacity onPress={pagAnt} style={[{width: windowWidth*0.1}]}>
-                    <Image source={{ uri: 'https://i1.sndcdn.com/artworks-Ezn5WMlxVoGDrSoy-l3qN8g-t500x500.jpg'}} 
-                        style={[{width: 100, height: 100}]}
+                    <Image source={{ uri: 'https://cdn-icons-png.freepik.com/512/800/800058.png'}} 
+                        style={[{width: 100, height: 100, transform: [{ scaleX: -1 }] }]}
                     />
                 </TouchableOpacity>
                 <TouchableOpacity onPress={proxPag} style={[{width: windowWidth*0.1}]}>
-                    <Image source={{ uri: 'https://i.scdn.co/image/ab67616100005174e1408498d7f528e3671616b1'}} 
+                    <Image source={{ uri: 'https://cdn-icons-png.freepik.com/512/800/800058.png'}} 
                         style={[{width: 100, height: 100}]}
                     />
                 </TouchableOpacity>
